ea765c1fcb304759f776b3900cc40a20857ce30e
Documenter.jl <documenter@juliadocs.github.io>
2023-03-22 11:37:05 +0000

    build based on ae6d4a2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BCRSimplexGaps","category":"page"},{"location":"#BCRSimplexGaps","page":"Home","title":"BCRSimplexGaps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BCRSimplexGaps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BCRSimplexGaps]","category":"page"}]
+}
