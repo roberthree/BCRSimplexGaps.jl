504a9d89bb94adb58e537f0fa8b34a59208990db
Robert Three <roberthree@proton.me>
2023-03-23 00:19:01 +1100

    update

--- src/BCRSimplexGaps.jl
+++ src/BCRSimplexGaps.jl
@@ -1,5 +1,85 @@
 module BCRSimplexGaps
 
-# Write your package code here.
+using Combinatorics
+using JuMP
+using HiGHS
+
+function simplex_numbers(d::Int, s::Int; max_level::Int = d)
+    [
+        p .- ones(Int, d + 1)
+        for p in partitions(s + d + 1, d + 1)
+        if sum(p .> 1) <= max_level + 1
+    ]
+end
+
+function simplex_instance(d::Int, s::Int; max_level::Int = d)
+    @assert 0 <= max_level <= d
+    n = max_level < d ? max_level + 2 : d + 1
+
+    sn = Dict(
+        o => simplex_numbers(n - 1, s + o; max_level)
+        for o in [0, 1]
+    )
+    sn_union = Iterators.flatten((sn[0], sn[1]))
+
+    model = Model(HiGHS.Optimizer; add_bridges = false)
+
+    # y(v)_i
+    @variable(model, y[sn_union, 1:n])
+
+    for p in sn_union
+        # 0-centric
+        @constraint(model, ones(n)' * y[p, :] == 0)
+
+        # v_i = v_j => y(v)_i = y(v)_j
+        for k in 0:sum(p)
+            indices = findall(==(k), p)
+            for i in indices[2:end]
+                factor = i < max_level + 2 ? 1 : (d - max_level)
+                @constraint(model, y[p, i] == factor * y[p, indices[1]])
+            end
+        end
+    end
+
+    # L1-embedding
+    for p₁ in sn[1], i in 1:sum(p₁ .> 0)
+        i == n || p₁[i] > p₁[i + 1] || continue
+        
+        p₀ = copy(p₁)
+        p₀[i] -= 1
+
+        r = @variable(model, [1:n])
+
+        for j in 1:n
+            @constraints(model, begin
+                +(y[p₀, j] - y[p₁, j]) <= r[j]
+                -(y[p₀, j] - y[p₁, j]) <= r[j]
+            end)
+        end
+
+        @constraint(model, ones(n)' * r <= 1)
+    end
+
+    @objective(model, Max, 1 * y[sn[0][1], 1])
+
+    model, sn, y
+end
+
+function compute_gap(d::Int, s::Int; max_level::Int = d)
+    model, sn, y = BCRSimplexGaps.simplex_instance(d, s; max_level)
+    # set_silent(model)
+    optimize!(model)
+    s_max = s * d / (d + 1)
+    s_opt = MOI.get(model, MOI.ObjectiveValue())
+    (
+        model = model,
+        sn    = sn,
+        y     = y,
+        s_max = s_max,
+        s_opt = s_opt,
+        gap   = s_max / s_opt,
+        time  = MOI.get(model, MOI.SolveTimeSec()),
+    )
+end
 
 end
--- src/simplex.jl
+++ src/simplex.jl
@@ -1,72 +1,12 @@
-using Combinatorics
-using JuMP
-using HiGHS
+using CSV
 using DataFrames
 using ProgressMeter
 
-function simplex_numbers(d::I, s::I; max_level::I = d) where {I <: Int}
-    [
-        p .- ones(I, d + 1)
-        for p in partitions(s + d + 1, d + 1)
-        if sum(p .> 1) <= max_level + 1
-    ]
-end
-
-function construct_simplex_instance(d::I, s::I; max_level::I = d) where {I <: Int}
-    @assert 0 <= max_level <= d
-    n = max_level < d ? max_level + 2 : d + 1
-
-    sn = Dict(
-        o => simplex_numbers(n - 1, s + o; max_level = max_level)
-        for o in [0, 1]
-    )
-    sn_union = Iterators.flatten((sn[0], sn[1]))
-
-    model = Model(HiGHS.Optimizer; add_bridges = false)
-    set_optimizer_attribute(model, "parallel", "on")
-
-    @variable(model, y[sn_union, 1:n])
-
-    for p in sn_union
-        @constraint(model, ones(n)' * y[p, :] == 0)
-
-        for k in 0:sum(p)
-            indices = findall(==(k), p)
-            for i in indices[2:end]
-                factor = i < max_level + 2 ? 1 : (d - max_level)
-                @constraint(model, y[p, i] == factor * y[p, indices[1]])
-            end
-        end
-    end
-
-    for p₁ in sn[1]
-        for i in 1:sum(p₁ .> 0)
-            i == n || p₁[i] > p₁[i + 1] || continue
-            
-            p₀ = copy(p₁)
-            p₀[i] -= 1
-
-            r = @variable(model, [1:n])
-
-            for j in 1:n
-                @constraints(model, begin
-                    +(y[p₀, j] - y[p₁, j]) <= r[j]
-                    -(y[p₀, j] - y[p₁, j]) <= r[j]
-                end)
-            end
-
-            @constraint(model, ones(n)' * r <= 1)
-        end
-    end
-
-    @objective(model, Max, 1 * y[sn[0][1], 1])
-
-    model, sn, y
-end
+include("BCRSimplexGaps.jl")
 
 simplex_parameters = [
     (d, s, l)
-    for d in [40, 500]
+    for d in [40]#, 500]
     for l in 3:3
     for s in max(l, 30):40
     # for d in max(s, 500):500
@@ -76,24 +16,20 @@ result = DataFrame(
     d = Int[],
     s = Int[],
     l = Int[],
-    s_max = AbstractFloat[],
-    s_opt = AbstractFloat[],
-    gap = AbstractFloat[],
-    time = AbstractFloat[],
+    s_max = Float64[],
+    s_opt = Float64[],
+    gap   = Float64[],
+    time  = Float64[],
 )
 
 @showprogress for (d, s, l) in simplex_parameters
-    model, sn, y = construct_simplex_instance(d, s; max_level = l)
-    # set_silent(model)
-    optimize!(model)
-    s_max = s * d / (d + 1)
-    s_opt = MOI.get(model, MOI.ObjectiveValue())
-    gap = s_max / s_opt
-    time = MOI.get(model, MOI.SolveTimeSec())
-    push!(result, [d, s, l, s_max, s_opt, gap, time])
+    printstyled("d = $(d), s = $(s), l = $(l)\n"; color = :red)
+    gap = BCRSimplexGaps.compute_gap(d, s; max_level = l)
+    push!(result, [d, s, l, gap.s_max, gap.s_opt, gap.gap, gap.time])
 end
 
 display(sort(result, :gap; rev = false))
+CSV.write("gaps.csv", result[:, [:d, :s, :l, :gap]])
 
 gaps = zeros((0, 0))
 
